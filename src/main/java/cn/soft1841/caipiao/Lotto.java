78b89b67b8efd283715502aef26d9d209dd8fda4
48038411 <48038411@qq.com>
2019-03-12 20:09:05 +0800

    大乐号码生成器

---
+++ src/main/java/cn/soft1841/caipiao/Lotto.java
@@ -0,0 +1,8 @@
+package cn.soft1841.caipiao;
+
+public class Buy {
+    public static void main(String[] args) {
+        int times = 7;
+        StringBuffer stringBuffer = new StringBuffer();
+    }
+}
